c8a0ffc52fe3390af5f1dcf929d39bb7239cb3a0
alonsoberrocal <71430464+alonsoberrocal@users.noreply.github.com>
2024-06-21 08:18:28 +0000

    prueba jlab

--- test1.py
+++ test1.py
@@ -2,8 +2,10 @@
 
 # Crea una variable llamada var1
 
-
+var1 = 2
 # Guarda en var1 el resultado de sumar 10 y 80
 
-
+var1 = 10 +80
 # Imprime por pantalla el valor almacenado en var1
+
+print (var1)
